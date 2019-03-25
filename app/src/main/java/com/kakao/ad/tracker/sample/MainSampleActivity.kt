bbe2c5f195bad6c599e01d3b6da6536e89c5c669
sgkang <unbalance09@gmail.com>
2019-03-25 15:12:38 +0900

    [AFADK-210] 이벤트 전송 샘플 코드 추가

--- app/src/main/java/com/kakao/ad/tracker/sample/MainSampleActivity.kt
+++ app/src/main/java/com/kakao/ad/tracker/sample/MainSampleActivity.kt
@@ -2,7 +2,11 @@ package com.kakao.ad.tracker.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.ad.common.json.*
 import com.kakao.ad.tracker.KakaoAdTracker
+import com.kakao.ad.tracker.send
+import kotlinx.android.synthetic.main.activity_main_sample.*
+import java.util.*
 
 class MainSampleActivity : AppCompatActivity() {
 
@@ -19,5 +23,110 @@ class MainSampleActivity : AppCompatActivity() {
         )
 
         setContentView(R.layout.activity_main_sample)
+
+        sendCompleteRegistrationEventButton.setOnClickListener { sendCompleteRegistrationEvent() }
+        sendPageViewEventButton.setOnClickListener { sendPageViewEvent() }
+        sendSearchEventButton.setOnClickListener { sendSearchEvent() }
+        sendViewContentEventButton.setOnClickListener { sendViewContentEvent() }
+        sendViewCartEventButton.setOnClickListener { sendViewCartEvent() }
+        sendPurchaseEventButton.setOnClickListener { sendPurchaseEvent() }
+        sendInAppPurchaseEventButton.setOnClickListener { sendInAppPurchaseEvent() }
+    }
+
+    /**
+     * 가입 이밴트(CompleteRegistration)를 전송합니다.
+     */
+    fun sendCompleteRegistrationEvent() {
+        val event = CompleteRegistration()
+        event.tag = "Tag" // 분류
+        event.send()
+    }
+
+    /**
+     * 페이지 방문 이밴트(PageView)를 전송합니다.
+     */
+    fun sendPageViewEvent() {
+        val event = PageView()
+        event.tag = "Tag" // 분류
+        event.send()
+    }
+
+    /**
+     * 검색 이벤트(Search)를 전송합니다.
+     */
+    fun sendSearchEvent() {
+        val event = Search()
+        event.tag = "Tag" // 분류
+        event.search_string = "Keyword" // 검색 문자열
+        event.send()
+    }
+
+    /**
+     * 콘텐츠/상품 조회 이벤트(ViewContent)를 전송합니다.
+     */
+    fun sendViewContentEvent() {
+        val event = ViewContent()
+        event.tag = "Tag" // 분류
+        event.content_id = "Content ID" // 상품 코드
+        event.send()
+    }
+
+    /**
+     * 장바구니 보기 이벤트(ViewCart)를 전송합니다.
+     */
+    fun sendViewCartEvent() {
+        val event = ViewCart()
+        event.tag = "Tag" // 분류
+        event.send()
+    }
+
+    /**
+     * 구매 이벤트(Purchase)를 전송합니다.
+     */
+    fun sendPurchaseEvent() {
+        val event = Purchase()
+        event.tag = "Tag" // 분류
+        event.products =  // 구매 상품 목록
+            listOf(
+                Product().also { product ->
+                    product.name = "Product 1"  // 상품명
+                    product.quantity = 1        // 개수
+                    product.price = 1.1         // 금액
+                },
+                Product().also { product ->
+                    product.name = "Product 2"  // 상품명
+                    product.quantity = 2        // 개수
+                    product.price = 2.2         // 금액
+                }
+            )
+        event.currency = Currency.getInstance(Locale.KOREA) // 통화코드(ISO-4217)
+        event.total_quantity = event.products?.sumBy { it.quantity } // 총 개수
+        event.total_price = event.products?.sumByDouble { it.price } // 총 금액
+        event.send()
+    }
+
+    /**
+     * 인앱 구매 이벤트(InAppPurchase)를 전송합니다.
+     */
+    fun sendInAppPurchaseEvent() {
+        val event = InAppPurchase()
+        event.tag = "Tag" // 분류
+        event.products =  // 구매 상품 목록
+            listOf(
+                Product().also { product ->
+                    product.name = "Product 1"  // 상품명
+                    product.quantity = 1        // 개수
+                    product.price = 1.1         // 금액
+                },
+                Product().also { product ->
+                    product.name = "Product 2"  // 상품명
+                    product.quantity = 2        // 개수
+                    product.price = 2.2         // 금액
+                }
+            )
+        event.currency = Currency.getInstance(Locale.KOREA) // 통화코드(ISO-4217)
+        event.total_quantity = event.products?.sumBy { it.quantity } // 총 개수
+        event.total_price = event.products?.sumByDouble { it.price } // 총 금액
+        event.send()
     }
 }
