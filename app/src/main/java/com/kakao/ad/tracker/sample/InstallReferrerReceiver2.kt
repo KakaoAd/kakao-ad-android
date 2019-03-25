9019c95d1527450ae86fe185cee561121de299cd
sgkang <unbalance09@gmail.com>
2019-03-25 15:07:15 +0900

    [AFADK-210] InstallReferrerReceiver 샘플 코드 추가

---
+++ app/src/main/java/com/kakao/ad/tracker/sample/InstallReferrerReceiver2.kt
@@ -0,0 +1,17 @@
+package com.kakao.ad.tracker.sample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InstallReferrerReceiver2 : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "com.android.vending.INSTALL_REFERRER") {
+            logv(
+                "\"${javaClass.simpleName}\" receive the \"com.android.vending.INSTALL_REFERRER\" action. " +
+                        "\"referrer\" is ${intent.getStringExtra("referrer")}"
+            )
+        }
+    }
+}
