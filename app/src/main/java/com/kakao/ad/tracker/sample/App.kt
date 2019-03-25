a73b78ca29450ddbe7c07e944485ad909a609b9b
sgkang <unbalance09@gmail.com>
2019-03-25 14:59:58 +0900

    [AFADK-210] KakaoAdTracker 초기화 관련 샘플 코드 추가

--- app/src/main/java/com/kakao/ad/tracker/sample/App.kt
+++ app/src/main/java/com/kakao/ad/tracker/sample/App.kt
@@ -1,5 +1,17 @@
 package com.kakao.ad.tracker.sample
 
 import android.app.Application
+import com.kakao.ad.tracker.KakaoAdTracker
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (!KakaoAdTracker.isInitialized) {
+            KakaoAdTracker.init(this, getString(R.string.kakao_ad_track_id))
+        }
+
+        logv("KakaoAdTracker.isInitialized? ${KakaoAdTracker.isInitialized}")
+    }
+}
--- app/src/main/java/com/kakao/ad/tracker/sample/MainSampleActivity.kt
+++ app/src/main/java/com/kakao/ad/tracker/sample/MainSampleActivity.kt
@@ -2,11 +2,22 @@ package com.kakao.ad.tracker.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.ad.tracker.KakaoAdTracker
 
 class MainSampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!KakaoAdTracker.isInitialized) {
+            KakaoAdTracker.init(this, getString(R.string.kakao_ad_track_id))
+        }
+
+        logAndToast(
+            "KakaoAdTracker.isInitialized? ${KakaoAdTracker.isInitialized}\n" +
+                    "KakaoAdTracker.version = ${KakaoAdTracker.VERSION}"
+        )
+
         setContentView(R.layout.activity_main_sample)
     }
 }
